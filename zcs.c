e0cc2bcef9108a3776c011793fc2392b493fc107
Joshua-M1 <jsmorency@hotmail.com>
2024-02-01 12:55:57 -0500

    started code for shutdown, query, and get attributes. waiting to see if a registry has been implemented but not pushed

--- zcs.c
+++ zcs.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int userType = 0;
+bool up = false;
 node *thisService;
 
 int zcs_init(int type) {
@@ -27,11 +29,12 @@ int zcs_start(char *name, zcs_attribute_t attr[], int num) {
         thisService->attr[i] = attr[i];
     }
 
+    up = true;
     //test statements to check that node is properly created
     //printf("service started\n");
     //printf("%s\n", thisService->attr[num-1].attr_name);
 
-    //put node into local library log if not already there, then send NOTIFICATION message to tell other nodes about existence/being UP
+    //put node into local library log, then send NOTIFICATION message to tell other nodes about existence/being UP
     //start HEARTBEAT
 
     return 0;
@@ -42,10 +45,29 @@ int zcs_post_ad(char *ad_name, char *ad_value) {
 }
 
 int zcs_query(char *attr_name, char *attr_value, char *node_names[], int namelen) {
+    // iterate over local registry
+    // for each node, check to see if it has an attribute with a specific value (until registry is exhausted or name array full, i.e. namelen entries)
+    // if yes, add to name array
+    // return number of valid nodes found
+
     return 0;
 }
 
 int zcs_get_attribs(char *name, zcs_attribute_t attr[], int *num) {
+    // for the provided service name, set the (preallocated) array to the number of attributes
+    // num originally size of the array. set num to attributes read. so you will always read <= num attributes
+    // if num < node->numOfAttr, return num attributes. otherwise return node->numOfAttr attributes
+
+    //get node from registry
+    node *n;
+
+    int i = 0;
+
+    for (; i < n->numOfAttr && i < *num; i++) {
+        attr[i] = n->attr[i];
+    }
+
+    *num = i;
     return 0;
 }
 
@@ -55,10 +77,15 @@ int zcs_listen_ad(char *name, zcs_cb_f cback) {
 
 int zcs_shutdown() {
     //check that service was already registered and that it is currently UP. if not fail
+    if (userType == 0 || !up) // || 
+        return -1;
 
-    //mark service as DOWN in local registry
-    //de allocate node memory
+    // mark service as DOWN in local registry
+    // if local registry is a linked list, can just remove the entry locally. other nodes will mark as down due to lack of heartbeat
+    // if service comes back, add to end of local registry and start heartbeating again. other nodes will see the service name in their registry and just mark it as up
 
+    up = false;
+    free(thisService);
     //stop HEARTBEAT
     return 0;
 }
