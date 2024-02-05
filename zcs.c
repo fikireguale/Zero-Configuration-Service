afa63b9fe0baec22b3df229fb2cb7f200af0f085
fikireguale <fikireguale@gmail.com>
2024-02-05 16:35:46 -0500

    incomplete zcs_init()

--- zcs.c
+++ zcs.c
@@ -4,15 +4,89 @@
 #include <string.h>
 #include <stdbool.h>
 #include "registry.h"
+#include "multicast.h"
 
 int userType = 0;
 bool up = false;
 node *thisService;
+char buffer[1000];
+int buffer_index = 0;
+pthread_mutex_t buffer_mutex;
+// linked list for specific nodes for listen_ad 
+int MAX_RETRIES = 3;
+
+void* receive_messages(void* arg, mcast_t *mcastNodeToApp) {
+    int num_bytes_received;
+    char temp_buffer[100];
+    time_t start_time, current_time;
+    time(&start_time);
+
+    while (1) {
+        // use global var for termination
+
+        if (multicast_check_receive(mcastNodeToApp)) {
+            num_bytes_received = multicast_receive(mcastNodeToApp, temp_buffer, sizeof(temp_buffer));
+            if (num_bytes_received > 0) {
+                pthread_mutex_lock(&buffer_mutex);
+                if (buffer_index + num_bytes_received < sizeof(buffer)) {
+                    memcpy(buffer + buffer_index, temp_buffer, num_bytes_received);
+                    buffer_index += num_bytes_received;
+                }
+                pthread_mutex_unlock(&buffer_mutex);
+            }
+        }
+    }
+
+    return NULL;
+}
+
 
 int zcs_init(int type) {
-    //send DISCOVERY message (makes other nodes send a NOTIFICATION message, i.e. share their existence)
     userType = type;
 
+    // Start network
+    int rport = atoi(argv[1]); // 5000
+    int sport = rport + rport + random() % rport; // 6000
+    // Channel used to send instructions to nodes via sport
+    mcast_t *mcastAppToNode = multicast_init("224.1.1.1", sport, 0);
+    // Channel used to receive responses from nodes via rport
+    mcast_t *mcastNodeToApp = multicast_init("224.1.1.1", 0, rport);
+
+    if (mcastAppToNode == NULL || mcastNodeToApp == NULL) {
+        return -1;
+    }
+
+    // App broadcasts DISCOVERY and listens for NOTIFICATIONs to update its local_registry
+    if (userType == 1) {
+        pthread_t listen_thread;
+        pthread_mutex_init(&buffer_mutex, NULL);
+
+        // Start listening thread
+        if (pthread_create(&listen_thread, NULL, receive_messages, NULL) != 0) {
+            perror("Failed to create listening thread");
+            return -1;
+        }
+
+        // Send 1 DISCOVERY msg
+        char msg[] = "zcs|01#";
+        multicast_send(mcastAppToNode, msg, strlen(msg));
+
+        // Read messages
+        pthread_mutex_lock(&buffer_mutex);
+        if (buffer_index > 0) {
+            printf("Buffer contents: %s\n", buffer);
+            //  update local registry
+            node *node_list = decode_buffer(buffer);
+            // for node in node_list
+            //  insertEntry(node* entry);
+
+            buffer_index = 0; // Reset
+            memset(buffer, 0, sizeof(buffer)); // Clear
+        } else {
+            printf("No nodes responded to %s msg.\r\n", msg);
+        }
+        pthread_mutex_unlock(&buffer_mutex);
+    }
     return 0;
 }
 
