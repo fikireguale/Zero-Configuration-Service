b3220ce9096e590cd171c8f06de51c1d11db8d08
fikireguale <fikireguale@gmail.com>
2024-02-06 20:43:31 -0500

    listen/respond thread + incomplete processData

--- zcs.c
+++ zcs.c
@@ -4,17 +4,21 @@
 #include <string.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include "registry.h"
 #include "multicast.h"
-
-#define BUFFERSIZE 1000
 
 int userType = 0;
 bool up = false;
 node *thisService;
+const int BUFFERSIZE = 1000;
 char buffer[BUFFERSIZE];
+// threads
 int readPt = 0;
 int writePt = 0;
+const int DATA_SIZE = 100;
+pthread_cond_t full;
+pthread_cond_t empty;
 pthread_mutex_t buffer_mutex;
 // linked list for specific nodes for zcs_listen_ad 
 int MAX_RETRIES = 3;
@@ -31,46 +35,117 @@ void* write_buffer(void* arg, mcast_t *mcastNodeToApp) {
             // shutdown procedure
             break;
         }
+        // While buffer is full, wait
+        //  since next position to write to is the current position to read from
+        while (((writePt + 1) % BUFFERSIZE) == readPt) {
+            pthread_cond_wait(&empty, &buffer_mutex);
+        }
         if (multicast_check_receive(mcastNodeToApp)) {
             num_bytes_received = multicast_receive(mcastNodeToApp, temp_buffer, sizeof(temp_buffer));
             if (num_bytes_received > 0) {
                 pthread_mutex_lock(&buffer_mutex);
-                // Add logic for: while buffer is full wait
                 if (writePt + num_bytes_received < sizeof(buffer)) {
                     // Write to buffer
+                    // void *memcpy(void *dest, const void *src, size_t n);
+                    // 'num_bytes_received + 1' to include the null terminator?
                     memcpy(buffer + writePt, temp_buffer, num_bytes_received);
-                    // Clear buffer
-                    memset(buffer, 0, sizeof(buffer));
-                    writePt = 0;
-                    //writePt += num_bytes_received % BUFFERSIZE; 
+                    writePt += num_bytes_received % BUFFERSIZE;
                 }
                 pthread_mutex_unlock(&buffer_mutex);
             }
         }
+        pthread_cond_signal(&full);
     }
-    // Add logic for: pthread_exit
+    // pthread_exit(NULL);
     return NULL;
 }
 
 void* read_buffer(void* arg, mcast_t *mcastAppToNode, mcast_t *mcastNodeToApp) {
-    // when reading the buffer, it's possible to misss a message..
+    // when reading the buffer, it's possible to miss a message if buffer is held here..
+    // process 
 
     while (1) {
         if (zcs_shutdown_ongoing == 1) {
             // shutdown procedure
             break;
         }
-
-        pthread_mutex_lock(&buffer_mutex);
-        // Add logic for: while buffer is empty wait
-        //   wait until something to read
-        // Add conditional statement to check buffer index
-        // Read/Pull data out of buffer
-        pthread_mutex_unlock(&buffer_mutex);
         
-    }
+        pthread_mutex_lock(&buffer_mutex);
+        // While buffer is empty wait
+        while (writePt == readPt) {
+            pthread_cond_wait(&full, &buffer_mutex);
+        }
+       
+        int num_bytes_to_read = writePt;
+        if (num_bytes_to_read > 0) {
+            char read_data[num_bytes_to_read];
+            // void *memcpy(void *dest, const void *src, size_t n);
+            memcpy(read_data, buffer, num_bytes_to_read);
 
+            // Process the data in read_data as needed (hopefully doesn't take too long..)
+                // Extract msg string from buffer
+                // Based on msg type redirect to different methods to handle
+            processData(read_data);
+
+            // Clear the buffer and reset writePt
+            memset(buffer, 0, BUFFERSIZE);
+            memset(read_data, 0, num_bytes_to_read);
+            writePt = 0;
+        }
+
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&buffer_mutex);
+    }
+    // pthread_exit(NULL);
     return NULL;
+}
+
+void processData(char *read_data) {
+    char *start = read_data;
+    char *end;
+    // strchr(): returns a pointer to '$' if it is found in start ie read_data
+    while ((start = strchr(start, '$')) != NULL) {
+        end = strchr(start, '#');
+        if (end == NULL) {
+            break; // No end marker found, stop processing
+        }
+
+        *end = '\0'; // Replace '#' to isolate the msg
+        char *msg = start + 1; // Start of actual msg (skip '$')
+
+        // Parse msg
+        if (strncmp(msg, "00", 2) == 0) {
+            // NOTIFICATION
+            // msg = "$msgType|nodeName|numOfAttr|status|attrName1=value1;attrName2=value2;...#"
+            // --> output a list of node structs
+            printf("NOTIFICATION message received\n");
+        
+        } else if (strncmp(msg, "01", 2) == 0) {
+            // DISCOVERY
+            // msg = "$01#"
+            // --> as a node, send a notif
+            printf("DISCOVERY message received\n");
+
+        } else if (strncmp(msg, "10", 2) == 0) {
+            // HEARTBEAT
+            char *nodeName = strtok(msg + 3, "|"); // +3 to skip "10|"
+            printf("HEARTBEAT from %s\n", nodeName);
+            // msg = "$10|nodeName#"
+            // --> ?
+            
+        } else if (strncmp(msg, "11", 2) == 0) {
+            // ADVERTISEMENT
+            // msg = "$msgType|nodeName|ad_name;ad_value#"
+            // -> execute zcs_listen_ad if headAd pointer isn't null
+            printf("ADVERTISEMENT message received\n");
+
+        } else {
+            // Process other message types
+            printf("UNKNOWN message received\n");
+
+        }
+        start = end + 1; // Go to next msg
+    }
 }
 
 
@@ -78,7 +153,7 @@ int zcs_init(int type) {
     userType = type;
 
     // Start network
-    int rport = atoi("5000"); // 5000
+    int rport = atoi(argv[1]); // 5000
     int sport = rport + rport + random() % rport; // 6000
     // Channel used to send instructions to nodes via sport
     mcast_t *mcastAppToNode = multicast_init("224.1.1.1", sport, 0);
@@ -89,7 +164,9 @@ int zcs_init(int type) {
         return -1;
     }
 
-    pthread_t listen_thread;
+    pthread_t listen_thread, respond_thread;
+    pthread_cond_init(&full, NULL);
+    pthread_cond_init(&empty, NULL);
     pthread_mutex_init(&buffer_mutex, NULL);
 
     // Start listening thread where buffer gets written
@@ -97,10 +174,17 @@ int zcs_init(int type) {
         perror("Failed to create listening thread");
         return -1;
     }
-    // App broadcasts DISCOVERY and listens for NOTIFICATIONs to update its local_registry
+
+     // Start response thread where buffer gets read
+    if (pthread_create(&respond_thread, NULL, read_buffer, NULL) != 0) {
+        perror("Failed to create response thread");
+        return -1;
+    }
+
+    // App broadcasts DISCOVERY (and listens for NOTIFICATIONs to update its local_registry)
     if (userType == ZCS_APP_TYPE) {
         // Send 1 DISCOVERY msg
-        char msg[] = "zcs|01#";
+        char msg[] = "$01#";
         multicast_send(mcastAppToNode, msg, strlen(msg));
         // wait 1 sec
     }
@@ -152,22 +236,7 @@ int zcs_query(char *attr_name, char *attr_value, char *node_names[], int namelen
     // if yes, add to name array
     // return number of valid nodes found
 
-    //send discovery? perhaps send discovery, then listen for a set period of time for notifications (e.g. wait 5s after last notification) to ensure all the responses (e.g. wait for a listening thread to join)
-
-    int count = 0;
-
-    for (int i = 0; i < getRegistryLength() && count < namelen; i++) {
-        registryEntry* entry = getEntryFromIndex(i);
-        if (!entry->up) continue; //if node is down, doesnt really make sense to consider it
-        for (int j = 0; j < entry->node->numOfAttr && count < namelen; j++) {
-            if (strcmp(attr_name, entry->node->attr[j].attr_name) == 0 && strcmp(attr_value, entry->node->attr[j].value) == 0) {
-                node_names[count] = entry->node->name;
-                count++;
-            }
-        }
-    }
-
-    return count;
+    return 0;
 }
 
 int zcs_get_attribs(char *name, zcs_attribute_t attr[], int *num) {
@@ -196,9 +265,10 @@ int zcs_listen_ad(char *name, zcs_cb_f cback) {
 
 int zcs_shutdown() {
     //check that service was already registered and that it is currently UP. if not fail
-    if (userType == 0) 
+    if (!up) 
         return -1;
 
+    // mark service as DOWN in local registry
     // if local registry is a linked list, can just remove the entry locally. other nodes will mark as down due to lack of heartbeat
     // if service comes back, add to end of local registry and start heartbeating again. other nodes will see the service name in their registry and just mark it as up
     removeEntryFromName(thisService->name);
@@ -208,15 +278,5 @@ int zcs_shutdown() {
 }
 
 void zcs_log() {
-    printf("-------- LOG START --------\n");
 
-    for (int i = 0; i < getRegistryLength(); i++) {
-        registryEntry* entry = getEntryFromIndex(i);
-        printf("Node name: %s\nSatus: %s\nLast Status Change: %f seconds ago\nAttributes:\n", entry->node->name, entry->up ? "UP" : "DOWN", difftime(entry->timeOfServiceChange, time(NULL)));
-        for (int j = 0; j < entry->node->numOfAttr; j++) {
-            printf("\t%s: %s\n", entry->node->attr[j].attr_name, entry->node->attr[j].value);
-        }
-    }
-
-    printf("--------- LOG END ---------\n");
 }
